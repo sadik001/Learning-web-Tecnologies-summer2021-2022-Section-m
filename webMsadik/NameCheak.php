4e447de93df1ad4338122fb1aa79bb6dc9660dd7
Md. sadik hossain chowdhury <62743890+sadik001@users.noreply.github.com>
2022-06-14 16:33:13 +0600

    sadik
    Lab Performance Task 14/06/2022

---
+++ webMsadik/NameCheak.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$name = $_POST['name'];
+
+//print_r($_POST);
+//echo $_POST'name';
+
+$length = strlen($name);
+
+
+if($name != null && $length >=2 && ctype_alpha($name) && ctype_alpha($name[0]))
+{
+    echo "login Success";
+}
+
+else echo "not success";
+
+
+?>
